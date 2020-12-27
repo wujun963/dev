859e7dce168716644a02d4fc4d88f2ebec3aba06
wujun963 <wj15939708149@163.com>
2020-12-27 21:58:27 +0800

    aaa

--- ali/src/test/java/Test.java
+++ ali/src/test/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("aaaaa");
         System.out.println("asdasdsadasd");
+        System.out.println("cccccccc");
     }
 }
